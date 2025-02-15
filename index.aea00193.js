e8d8c27da641b017c1c15b5e7e1a8f317598b9fa
Kyrylo Bondarenko <dejsk35@gmail.com>
2025-02-15 19:09:41 +0200

    make build

---
+++ index.aea00193.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector("img"),l=e.offsetWidth/2-t.offsetWidth/2-10;t.style.top=l+"px",t.style.left=l+"px";
+//# sourceMappingURL=index.aea00193.js.map
